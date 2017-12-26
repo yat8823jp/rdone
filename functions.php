e869dbfdc88ba6b9dfce8b0157d6db544f30c874
yat8823jp <yat@yat-net.com>
2017-12-26 18:51:15 +0900

    thumnail-size

--- functions.php
+++ functions.php
@@ -41,8 +41,72 @@
 /**
  * サムネイルサイズ拡張
  **/
-add_image_size( 'img320', 320, 0, true );
+add_image_size( 'img320', 320, 400, true );
+function cropTopLeft( $first, $orig_w, $orig_h, $dest_w, $dest_h, $crop ) {
+	if ( null !== $output )
+		return $output;
 
+	if ( $crop ) {
+		// crop the largest possible portion of the original image that we can size to $dest_w x $dest_h
+		$aspect_ratio = $orig_w / $orig_h;
+		$new_w = min($dest_w, $orig_w);
+		$new_h = min($dest_h, $orig_h);
+
+		if ( ! $new_w ) {
+			$new_w = (int) round( $new_h * $aspect_ratio );
+		}
+
+		if ( ! $new_h ) {
+			$new_h = (int) round( $new_w / $aspect_ratio );
+		}
+
+		$size_ratio = max($new_w / $orig_w, $new_h / $orig_h);
+
+		$crop_w = round($new_w / $size_ratio);
+		$crop_h = round($new_h / $size_ratio);
+
+		if ( ! is_array( $crop ) || count( $crop ) !== 2 ) {
+			$crop = array( 'center', 'center' );
+		}
+
+		list( $x, $y ) = $crop;
+
+		if ( 'left' === $x ) {
+			$s_x = 0;
+		} elseif ( 'right' === $x ) {
+			$s_x = $orig_w - $crop_w;
+		} else {
+			$s_x = 0;
+		}
+
+		if ( 'top' === $y ) {
+			$s_y = 0;
+		} elseif ( 'bottom' === $y ) {
+			$s_y = $orig_h - $crop_h;
+		} else {
+			$s_y = 0;
+		}
+	} else {
+		// don't crop, just resize using $dest_w x $dest_h as a maximum bounding box
+		$crop_w = $orig_w;
+		$crop_h = $orig_h;
+
+		$s_x = 0;
+		$s_y = 0;
+
+		list( $new_w, $new_h ) = wp_constrain_dimensions( $orig_w, $orig_h, $dest_w, $dest_h );
+	}
+
+	// if the resulting image would be the same size or larger we don't want to resize it
+	if ( $new_w >= $orig_w && $new_h >= $orig_h && $dest_w != $orig_w && $dest_h != $orig_h ) {
+		return false;
+	}
+
+	// the return array matches the parameters to imagecopyresampled()
+	// int dst_x, int dst_y, int src_x, int src_y, int dst_w, int dst_h, int src_w, int src_h
+	return array( 0, 0, (int) $s_x, (int) $s_y, (int) $new_w, (int) $new_h, (int) $crop_w, (int) $crop_h );
+}
+add_filter( 'image_resize_dimensions', 'cropTopLeft', 10, 6 );
 
 //====================================================================================
 // クロスドメイン問題の解決 - for SAKURA Internet
